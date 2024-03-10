fb58a5f376785d777e2b634f702d1cb777699069
vilem <ricanvi1@uhk.cz>
2024-03-10 19:41:22 +0100

    Test Commit

---
+++ src/test/java/com/example/pawsdemo/PawSdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawSdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
