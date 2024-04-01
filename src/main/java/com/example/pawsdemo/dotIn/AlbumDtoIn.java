30a11281fea174aaaa7d7808cc9600fcb8a62222
Hakurisa <71612411+Hakurisa@users.noreply.github.com>
2024-04-01 21:09:59 +0200

    Uploading the song to database

---
+++ src/main/java/com/example/pawsdemo/dotIn/AlbumDtoIn.java
@@ -0,0 +1,4 @@
+package com.example.pawsdemo.dotIn;
+
+public class AlbumDtoIn {
+}
--- src/main/java/com/example/pawsdemo/services/SkladbaService.java
+++ src/main/java/com/example/pawsdemo/services/SkladbaService.java
@@ -6,20 +6,28 @@ import com.backblaze.b2.client.contentSources.B2ContentSource;
 import com.backblaze.b2.client.exceptions.B2Exception;
 import com.backblaze.b2.client.structures.B2UploadFileRequest;
 import com.example.pawsdemo.dotIn.SkladbaDtoIn;
+import com.example.pawsdemo.models.AlbumEntity;
 import com.example.pawsdemo.models.SkladbaEntity;
+import com.example.pawsdemo.repository.AlbumRepository;
+import com.example.pawsdemo.repository.SkladbaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalTime;
+import java.util.TimeZone;
 
 import static com.backblaze.b2.client.structures.B2UploadFileRequest.builder;
 
@@ -35,6 +43,11 @@ public class SkladbaService {
     @Value("${backblaze.b2.fileUrl}")
     private String fileUrl;
 
+    @Autowired
+    private SkladbaRepository skladbaRepo;
+    @Autowired
+    private AlbumRepository albumRepo;
+
     private static final Logger logger = LoggerFactory.getLogger(SkladbaService.class);
 
 
@@ -44,20 +57,42 @@ public class SkladbaService {
             String songFileName = song.getOriginalFilename();
             String coverImageFileName = coverImage.getOriginalFilename();
 
-            int durationInSeconds = getSongDurationInSeconds(song);
+            int durationInSeconds = getSongDurationInSeconds(song); //result - 158
             logger.info("Duration in seconds: " + durationInSeconds);
+            long milliseconds = durationInSeconds * 1000L;
 
+            int seconds = durationInSeconds % 60;
+            int minutes = (durationInSeconds / 60) % 60;
+            int hours = durationInSeconds / 3600;
+
+            Date date = new Date(milliseconds);
+            Time songLength = new Time(hours, minutes, seconds);
+            logger.info("Song length - time format: " + songLength);
             //uploading to cloud storage
+            //TODO: do this after saving to DB, get the song's id, make a new folder in the bucket - "songs/{ID}" and save to there
             uploadToB2(songFileName, song.getBytes());
             uploadToB2(coverImageFileName, coverImage.getBytes());
 
             //saving the song into database
             final SkladbaEntity skladbaEntity = new SkladbaEntity();
+            final AlbumEntity album = new AlbumEntity();
             skladbaEntity.setAudioslozka(fileUrl + songFileName);
             skladbaEntity.setCoverimage(fileUrl + coverImageFileName);
             skladbaEntity.setJmeno(skladba.getJmeno());
-            skladbaEntity.setDelka(new Time(durationInSeconds));
+            skladbaEntity.setDelka(songLength);
             skladbaEntity.setPocetprehrani(0);
+
+            //FIXME: hack - there's no implementation of an album yet, so I make one up
+            album.setCoverImage(fileUrl + coverImageFileName);
+            album.setPocetskladeb(1);
+            album.setNazev(skladba.getJmeno());
+            album.setPublikovano((byte) 1);
+            album.setPopis("Popis...");
+            album.setDelka(songLength);
+
+            albumRepo.save(album);
+            skladbaEntity.setAlbumId(album.getAlbumId());
+            skladbaRepo.save(skladbaEntity);
         } catch (IOException ex) {
             throw new RuntimeException("Could not store file.");
         }
@@ -86,11 +121,34 @@ public class SkladbaService {
      */
     private int getSongDurationInSeconds(MultipartFile song) {
         try {
-            AudioFile audioFile = AudioFileIO.read((File) song);
+            AudioFile audioFile = AudioFileIO.read(convertMultipartFileToFile(song));
             AudioHeader audioHeader = audioFile.getAudioHeader();
+            removeFileFromDirectory(song);
             return audioHeader.getTrackLength();
         } catch (Exception e) {
-            throw new RuntimeException("Error while getting song duration.");
+            throw new RuntimeException("Error while getting song duration." + e);
         }
+    }
+
+    private File convertMultipartFileToFile(MultipartFile multipartFile) {
+        try {
+            // Create a new File
+            File file = new File("./target/" + multipartFile.getOriginalFilename());
+
+            // Create a FileOutputStream for the file
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                // Copy the contents of the MultipartFile to the FileOutputStream
+                FileCopyUtils.copy(multipartFile.getInputStream(), fos);
+            }
+
+            return file;
+        } catch (IOException e) {
+            throw new RuntimeException("Error converting MultipartFile to File: " + e.getMessage(), e);
+        }
+    }
+
+    private boolean removeFileFromDirectory(MultipartFile multipartFile) {
+        File file = new File("./target/" + multipartFile.getOriginalFilename());
+        return file.delete();
     }
 }
