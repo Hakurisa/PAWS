d2b2eb1d35d241c8de06b630a183b1d3ba1c61c1
Hakurisa <71612411+Hakurisa@users.noreply.github.com>
2024-04-21 21:45:35 +0200

    Žánry skript
    GPT generated because i'm lazy

---
+++ Database/Zanr.sql
@@ -0,0 +1,16 @@
+INSERT INTO Zanr (Nazev, Anotace) VALUES
+                                      ('Pop', 'Populární hudba vhodná pro široké publikum.'),
+                                      ('Rock', 'Žánr založený na kytarové hudbě s výrazným rytmem.'),
+                                      ('Hip Hop', 'Hudební žánr spojený s rapem a beatovou hudbou.'),
+                                      ('R&B', 'Rhythm and blues - hudba spojující prvky soulu, funku a hip hopu.'),
+                                      ('Jazz', 'Hudební žánr založený na improvizaci a rytmu.'),
+                                      ('Klasická hudba', 'Tradiční hudba založená na orchestrálních skladbách a kompozicích.'),
+                                      ('Elektronická hudba', 'Hudba vytvářená pomocí elektronických nástrojů a technologií.'),
+                                      ('Metal', 'Agresivní hudba charakterizovaná silnými kytarovými riffy a vokály.'),
+                                      ('Indie', 'Nezávislá hudba produkovaná mimo hlavní hudební průmysl.'),
+                                      ('Reggae', 'Hudební styl pocházející z Jamajky, charakterizovaný úspornými rytmy a společenskými texty.'),
+                                      ('Blues', 'Hudební žánr vycházející z afroamerického folku.'),
+                                      ('Folk', 'Tradiční lidová hudba spojená s kulturním dědictvím daného regionu.'),
+                                      ('Country', 'Americký hudební žánr spojený s venkovským životním stylem.'),
+                                      ('Punk', 'Energetický hudební žánr charakterizovaný jednoduchými akordy a dravým stylem hraní.'),
+                                      ('Funk', 'Hudební styl charakterizovaný výrazným rytmem a silnými basovými liniemi.');
