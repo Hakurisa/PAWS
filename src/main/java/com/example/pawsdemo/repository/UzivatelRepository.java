48fe648a0d5e8323824fab515c0274de8b402dfe
radan157 <35270411+radan157@users.noreply.github.com>
2024-04-28 11:58:20 +0200

    createplaylist styling + some general stuff

--- src/main/java/com/example/pawsdemo/repository/UzivatelRepository.java
+++ src/main/java/com/example/pawsdemo/repository/UzivatelRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 @Repository
